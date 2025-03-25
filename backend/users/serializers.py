b80d477ea91b2798369e7ee41e9370ca6e27e555
zhanyysh <tynybekovjanyshbek@gmail.com>
2025-03-25 18:42:10 +0600

    userinfo

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -4,7 +4,7 @@ from .models import User, Transaction, CategoryAmount
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'password']
+        fields = ['id', 'username', 'email', 'password']
         extra_kwargs = {
             'password': {'write_only': True},
             'id': {'read_only': True}
